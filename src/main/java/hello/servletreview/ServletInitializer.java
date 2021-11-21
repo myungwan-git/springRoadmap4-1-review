3647a2f7bf04f1adabb1ad575fab1947e9b08684
myungwan-git <nsubway1070635@gmail.com>
2021-11-21 14:27:36 +0900

    20211121 spring roadmap4-1 review START

---
+++ src/main/java/hello/servletreview/ServletInitializer.java
@@ -0,0 +1,13 @@
+package hello.servletreview;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServletreviewApplication.class);
+	}
+
+}
---
+++ src/main/java/hello/servletreview/ServletreviewApplication.java
@@ -0,0 +1,13 @@
+package hello.servletreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletreviewApplication.class, args);
+	}
+
+}
